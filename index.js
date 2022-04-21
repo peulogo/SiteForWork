e8469a654b7dc4f3c3ddb5ecde25e9fba867dceb
peulogo <99072707+peulogo@users.noreply.github.com>
2022-04-21 19:14:17 +0300

    1.0.0

---
+++ index.js
@@ -0,0 +1,25 @@
+// window.onload = () => {
+
+// let acceptButton = document.querySelector('.toForm');
+// let firstForm = document.querySelector('.modal');
+// let overlay = document.querySelector('.overlay');
+// let formCloseButton = document.querySelector('.modal_close-button > button');
+
+
+// let getForm = e => {
+//     e.preventDefault();
+//     firstForm.classList.add('active');
+//     overlay.classList.add('active');
+//     overlay.addEventListener('click', closeForm)
+// }
+
+// let closeForm = e => { 
+//     firstForm.classList.remove('active');
+//     overlay.classList.remove('active');
+//     overlay.removeEventListener('click', closeForm)
+// }
+
+
+// acceptButton.addEventListener('click', getForm);
+// formCloseButton.addEventListener('click', closeForm);
+// }
